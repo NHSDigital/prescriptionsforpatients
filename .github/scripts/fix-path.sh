a80a74387d7aa0574cfda3803b27c6e9179f7f84
Phil Gee <originalphil@gmail.com>
2023-05-24 10:21:21 +0000

    Added missing forward slashes to PATH additions.

--- .github/scripts/fix-path.sh
+++ .github/scripts/fix-path.sh
@@ -2,11 +2,7 @@
 
 echo "> Adding dependencies to PATH"
 
-{
-  echo "$HOME/.asdf/bin/"
-  "$HOME/.asdf/installs/poetry/1.3.1/bin"
-  "$HOME/.asdf/installs/python/3.8.15/bin"
-} >> "$GITHUB_PATH"
+echo "$HOME/.asdf/bin/:$HOME/.asdf/installs/poetry/1.3.1/bin/:$HOME/.asdf/installs/python/3.8.15/bin/" >> "$GITHUB_PATH"
 
 # Got this far? Then we are good
 echo "Finished running $(basename "$0")"
