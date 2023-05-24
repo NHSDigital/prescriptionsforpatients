ad73ff5a6233553988724b0ab900020ca1e76f55
Phil Gee <originalphil@gmail.com>
2023-05-24 10:09:11 +0000

    Following documentation.

--- .github/scripts/fix-path.sh
+++ .github/scripts/fix-path.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-echo "> Fixing PATH by adding dependencies installed via asdf"
+echo "> Adding dependencies to PATH"
 
-echo "/home/ubuntu/.asdf/installs/poetry/1.3.1/bin" >> "$GITHUB_PATH"
-echo "/home/ubuntu/.asdf/installs/python/3.8.15/bin" >> "$GITHUB_PATH"
+{
+  echo "$HOME/.asdf/bin/"
+  "$HOME/.asdf/installs/poetry/1.3.1/bin"
+  "$HOME/.asdf/installs/python/3.8.15/bin"
+} >> "$GITHUB_PATH"
 
 # Got this far? Then we are good
 echo "Finished running $(basename "$0")"
