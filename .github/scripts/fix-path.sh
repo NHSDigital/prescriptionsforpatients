622f7d0825b97cc387da6a11595bd748b695a5c3
Phil Gee <originalphil@gmail.com>
2023-05-24 09:24:42 +0000

    Recommended way of adding to PATH.

--- .github/scripts/fix-path.sh
+++ .github/scripts/fix-path.sh
@@ -2,8 +2,8 @@
 
 echo "> Fixing PATH by adding dependencies installed via asdf"
 
-PATH="/home/ubuntu/.asdf/installs/poetry/1.3.1/bin:$PATH"
-export PATH="/home/ubuntu/.asdf/installs/python/3.8.15/bin:$PATH"
+echo "/home/ubuntu/.asdf/installs/poetry/1.3.1/bin" >> "$GITHUB_PATH"
+echo "/home/ubuntu/.asdf/installs/python/3.8.15/bin" >> "$GITHUB_PATH"
 
 # Got this far? Then we are good
 echo "Finished running $(basename "$0")"
