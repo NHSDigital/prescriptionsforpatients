0e46354b6f252d324cf7dd38aebf8ce5dd1a69c6
Jack Spagnoli <114365987+JackSpagnoliNHS@users.noreply.github.com>
2023-06-22 13:54:24 +0000

    updates error handling to new format

--- packages/middleware/src/index.js
+++ packages/middleware/src/index.js
@@ -1,6 +1,8 @@
 module.exports = ({logger = console, level = "error", filter = () => true} = {}) => ({
   onError: async (handler) => {
-    const {error} = handler
+    const error = handler.error ?? {}
+    const requestId = handler.event.requestContext?.requestId ?? null
+    const requestTimeEpoch = handler.event.requestContext?.requestTimeEpoch ?? null
 
     // if there are a `statusCode` and an `error` field
     // this is a valid http error object
@@ -22,14 +24,34 @@ module.exports = ({logger = console, level = "error", filter = () => true} = {})
       )
     }
 
-    handler.response = {
+    const response = {
       resourceType: "OperationOutcome",
       issue: [
         {
-          severity: "error",
-          code: "informational"
+          severity: "fatal",
+          code: "exception",
+          details: {
+            coding: [
+              {
+                system: "https://fhir.nhs.uk/CodeSystem/http-error-codes",
+                code: "SERVER_ERROR",
+                display: "500: The Server has encountered an error processing the request."
+              }
+            ]
+          }
         }
       ]
     }
+
+    if(requestId!==null){
+      response.id = requestId
+    }
+    if(requestTimeEpoch!==null){
+      response.meta = {
+        lastUpdated: requestTimeEpoch
+      }
+    }
+
+    handler.response = response
   }
 })
