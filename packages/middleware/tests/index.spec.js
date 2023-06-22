b776e18ccd97e0d3c55a46eb5c7b83f18c02bf07
Jack Spagnoli <114365987+JackSpagnoliNHS@users.noreply.github.com>
2023-06-22 09:50:57 +0000

    removes unused tests

--- packages/middleware/tests/index.spec.js
+++ packages/middleware/tests/index.spec.js
@@ -24,27 +24,6 @@ test("Middleware returns valid FHIR with logging disabled", async () => {
   })
 })
 
-// test("Middleware doesn't log 404 errors - they happen a lot", async () => {
-//     const mockLogger = {
-//         error: jest.fn(() => {}),
-//     };
-
-//     const handler = middy(() => {
-//         throw new createError.NotFound('File not found');
-//     });
-
-//     handler.use(middleware({ logger: mockLogger, filter: (err) => err.statusCode >= 500 }));
-
-//     const response = await handler({}, {});
-//     const { statusCode, message, stack } = JSON.parse(response.body);
-
-//     expect(statusCode).toBe(404);
-//     expect(message).toBe('File not found');
-//     expect(stack).toBeUndefined();
-
-//     expect(mockLogger.error).toHaveBeenCalledTimes(0);
-// });
-
 test("Middleware logs all error details and returns valid FHIR", async () => {
   const mockLogger = {
     error: jest.fn(() => {})
@@ -81,27 +60,3 @@ test("Middleware logs all error details and returns valid FHIR", async () => {
   expect(errorObject.error.expose).toBe(false)
   expect(errorObject.error.stack).not.toBeNull()
 })
-
-// test('Keep data already present in response', async () => {
-//     const handler = middy(async () => {
-//         throw error;
-//     });
-
-//     handler.use(middleware({ logger: false }));
-
-//     // eslint-disable-next-line no-shadow
-//     handler.onError(async (handler) => {
-//         // eslint-disable-next-line no-param-reassign
-//         handler.response = {
-//             headers: {
-//                 someHeader: 'someValue',
-//             },
-//         };
-//     });
-
-//     await expect(handler({}, {})).resolves.toMatchObject({
-//         body: JSON.stringify({ statusCode: 404, message: 'File not found' }),
-//         statusCode: 404,
-//         headers: { someHeader: 'someValue' },
-//     });
-// });
