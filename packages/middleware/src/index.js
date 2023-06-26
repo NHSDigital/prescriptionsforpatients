c333c384134fab81d3b91ca6742ff8259a6b6518
Jack Spagnoli <114365987+JackSpagnoliNHS@users.noreply.github.com>
2023-06-26 11:45:30 +0100

    AEA-3313 Return FHIR for System Errors (#63)

---
+++ packages/middleware/src/index.js
@@ -0,0 +1,63 @@
+module.exports = ({logger = console, level = "error", filter = () => true} = {}) => ({
+  onError: async (handler) => {
+    const error = handler.error ?? {}
+    const requestId = handler.event.requestContext?.requestId ?? null
+    const timeEpoch = handler.event.requestContext?.timeEpoch ?? null
+
+    // if there are a `statusCode` and an `error` field
+    // this is a valid http error object
+    if (filter(error) && typeof logger[level] === "function") {
+      logger[level](
+        {
+          error: (({name, message, stack, details, cause, status, statusCode, expose}) => ({
+            name,
+            message,
+            stack,
+            details,
+            cause,
+            status,
+            statusCode,
+            expose
+          }))(error)
+        },
+        `${error.name ?? ""}: ${error.message ?? ""}`
+      )
+    }
+
+    const responseBody = {
+      resourceType: "OperationOutcome",
+      issue: [
+        {
+          severity: "fatal",
+          code: "exception",
+          details: {
+            coding: [
+              {
+                system: "https://fhir.nhs.uk/CodeSystem/http-error-codes",
+                code: "SERVER_ERROR",
+                display: "500: The Server has encountered an error processing the request."
+              }
+            ]
+          }
+        }
+      ]
+    }
+
+    if(requestId!==null){
+      responseBody.id = requestId
+    }
+    if(timeEpoch!==null){
+      responseBody.meta = {
+        lastUpdated: timeEpoch
+      }
+    }
+
+    handler.response = {
+      statusCode: 500,
+      body: JSON.stringify(responseBody),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  }
+})
