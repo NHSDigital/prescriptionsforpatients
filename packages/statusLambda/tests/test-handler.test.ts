ff023bb0b6a43c4d0b42aed03c1d79645694fb1a
Jack Spagnoli <114365987+JackSpagnoliNHS@users.noreply.github.com>
2023-06-28 13:24:59 +0000

    adds status check tests

---
+++ packages/statusLambda/tests/test-handler.test.ts
@@ -0,0 +1,110 @@
+import {APIGatewayProxyEvent, APIGatewayProxyResult} from "aws-lambda"
+import {handler} from "../src/app"
+import {expect, describe, it} from "@jest/globals"
+import {ContextExamples} from "@aws-lambda-powertools/commons"
+
+const dummyContext = ContextExamples.helloworldContext
+
+describe("Unit test for status check", function () {
+  it("verifies pass response", async () => {
+    const event: Partial<APIGatewayProxyEvent> = {
+      queryStringParameters: {
+        returnType: "pass"
+      }
+    }
+    const result: APIGatewayProxyResult = (await handler(
+      event as APIGatewayProxyEvent,
+      dummyContext
+    )) as APIGatewayProxyResult
+
+    expect(result.statusCode).toEqual(200)
+    expect(result.body).toEqual(
+      JSON.stringify({
+        status: "pass",
+        commitId: "Some version number",
+        checks: [{}]
+      })
+    )
+  })
+
+  it("verifies warn response", async () => {
+    const event: Partial<APIGatewayProxyEvent> = {
+      queryStringParameters: {
+        returnType: "warn"
+      }
+    }
+    const result: APIGatewayProxyResult = (await handler(
+      event as APIGatewayProxyEvent,
+      dummyContext
+    )) as APIGatewayProxyResult
+
+    expect(result.statusCode).toEqual(200)
+    expect(result.body).toEqual(
+      JSON.stringify({
+        status: "warn",
+        commitId: "Some version number",
+        checks: [
+          {
+            message: "Warning about something non-critical"
+          }
+        ]
+      })
+    )
+  })
+
+  it("verifies error response", async () => {
+    const event: Partial<APIGatewayProxyEvent> = {
+      queryStringParameters: {
+        returnType: "error"
+      }
+    }
+    const result: APIGatewayProxyResult = (await handler(
+      event as APIGatewayProxyEvent,
+      dummyContext
+    )) as APIGatewayProxyResult
+
+    expect(result.statusCode).toEqual(200)
+    expect(result.body).toEqual(
+      JSON.stringify({
+        status: "error",
+        commitId: "Some version number",
+        checks: [
+          {
+            message: "There is an error somewhere"
+          },
+          {
+            message: "And another one somewhere else"
+          }
+        ]
+      })
+    )
+  })
+
+  it("verifies default response", async () => {
+    const event: Partial<APIGatewayProxyEvent> = {
+      queryStringParameters: {
+        returnType: "something else"
+      }
+    }
+    const result: APIGatewayProxyResult = (await handler(
+      event as APIGatewayProxyEvent,
+      dummyContext
+    )) as APIGatewayProxyResult
+
+    expect(result.statusCode).toEqual(200)
+    expect(result.body).toEqual(
+      JSON.stringify({
+        status: "error",
+        commitId: "Some version number",
+        checks: [
+          {
+            message: "There is an error somewhere"
+          },
+          {
+            message: "And another one somewhere else"
+          }
+        ]
+      })
+    )
+  })
+})
