090155da41bc66905fe429da6d82408a74e1ebb6
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2023-08-03 16:36:40 +0000

    validate response and nhs number

--- packages/spineClient/src/live-spine-client.ts
+++ packages/spineClient/src/live-spine-client.ts
@@ -32,16 +32,30 @@ export class LiveSpineClient implements SpineClient {
       }
 
       const address = this.getSpineEndpoint("mm/patientfacingprescriptions")
-      logger.info(`making request to ${address}`)
+      // nhsd-nhslogin-user looks like P9:9912003071
+      const nhsNumber = this.extractNHSNumber(inboundHeaders["nhsd-nhslogin-user"])
+      logger.info(`nhsNumber: ${nhsNumber}`)
+
       const queryParams = {
-        nhsNumber: inboundHeaders["nhsd-nhslogin-user"]?.split(":")[1]
+        nhsNumber: nhsNumber
       }
+      logger.info(`making request to ${address}`)
       const response = await axios.get<string>(address, {
         headers: outboundHeaders,
         params: queryParams,
         httpsAgent: this.httpsAgent
       })
 
+      if (response.data["statusCode"] !== 0) {
+        logger.error("Unsuccessful status code response from spine", {
+          response: {
+            data: response.data,
+            status: response.status,
+            Headers: response.headers
+          }
+        })
+        throw "Unsuccessful status code response from spine"
+      }
       return response
     } catch (error) {
       if (axios.isAxiosError(error)) {
@@ -85,5 +99,55 @@ export class LiveSpineClient implements SpineClient {
   async getStatus(logger: Logger): Promise<StatusCheckResponse> {
     const url = this.getSpineEndpoint("healthcheck")
     return serviceHealthCheck(url, logger, this.httpsAgent)
+  }
+
+  private extractNHSNumber(nhsloginUser: string | undefined): string {
+    if (nhsloginUser === undefined || nhsloginUser === null) {
+      throw "NHS Number not passed in"
+    }
+    let nhsNumber = nhsloginUser.split(":")[1]
+    if (
+      nhsNumber === undefined ||
+      nhsNumber === null ||
+      isNaN(Number(nhsNumber)) ||
+      nhsNumber.toString().length !== 10
+    ) {
+      throw "NHS Number failed preflight checks"
+    }
+
+    // convert numbers to strings, for internal consistency
+    if (Number.isInteger(nhsNumber)) {
+      nhsNumber = nhsNumber.toString()
+    }
+
+    // Step 1: Multiply each of the first 9 numbers by (11 - position indexed from 1)
+    // Step 2: Add the results together
+    // Step 3: Divide the total by 11 to get the remainder
+    const nhsNumberAsArray: string[] = nhsNumber.split("")
+    const remainder = nhsNumberAsArray.slice(0, 9).map(this.multiplyByPosition).reduce(this.addTogether, 0) % 11
+
+    let checkDigit = 11 - remainder
+
+    // replace 11 for 0
+    if (checkDigit === 11) {
+      checkDigit = 0
+    }
+
+    const providedCheckDigit = nhsNumberAsArray[9]
+
+    // Do the check digits match?
+    if (checkDigit !== Number(providedCheckDigit)) {
+      throw "invalid check digit in NHS number"
+    }
+    return nhsNumber
+  }
+
+  private multiplyByPosition(digit: string, index: number) {
+    // multiple each digit by 11  minus its position (indexed from 1)
+    return parseInt(digit) * (11 - (index + 1))
+  }
+
+  private addTogether(previousValue: number, currentValue: number) {
+    return previousValue + currentValue
   }
 }
