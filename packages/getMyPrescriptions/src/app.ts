0fb1518237b7023d29ff80f539a148a00e3e3ea5
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2023-08-07 07:55:20 +0000

    do not need eslint hints anymore

--- packages/getMyPrescriptions/src/app.ts
+++ packages/getMyPrescriptions/src/app.ts
@@ -20,7 +20,6 @@ const logger = new Logger({serviceName: "getMyPrescriptions"})
  */
 
 const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
   const spineClient = createSpineClient()
 
   try {
