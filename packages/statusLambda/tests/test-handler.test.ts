99582f6b649aeb919f75add84aeb1aa4173eef11
Adam Brown <adam.brown41@nhs.net>
2023-08-11 13:21:29 +0100

    AEA-3348 - Enhance API AWS Lambda Logs  (#134)
    Updates the logger to add a variety of trace ids:
    
    - nhsd-correlation-id
    - x-request-id
    - nhsd-request-id
    - x-correlation-id
    - apigw-request-id

--- packages/statusLambda/tests/test-handler.test.ts
+++ packages/statusLambda/tests/test-handler.test.ts
@@ -2,19 +2,70 @@ import {APIGatewayProxyEvent, APIGatewayProxyResult} from "aws-lambda"
 import {handler} from "../src/app"
 import {expect, describe, it} from "@jest/globals"
 import {ContextExamples} from "@aws-lambda-powertools/commons"
+import {Logger} from "@aws-lambda-powertools/logger"
 
 const dummyContext = ContextExamples.helloworldContext
+const mockEvent: APIGatewayProxyEvent = {
+  httpMethod: "get",
+  body: "",
+  headers: {
+    "nhsd-correlation-id": "test-request-id.test-correlation-id.rrt-5789322914740101037-b-aet2-20145-482635-2",
+    "x-request-id": "test-request-id",
+    "nhsd-request-id": "test-request-id",
+    "x-correlation-id": "test-correlation-id"
+  },
+  isBase64Encoded: false,
+  multiValueHeaders: {},
+  multiValueQueryStringParameters: {},
+  path: "/hello",
+  pathParameters: {},
+  queryStringParameters: {},
+  requestContext: {
+    accountId: "123456789012",
+    apiId: "1234",
+    authorizer: {},
+    httpMethod: "get",
+    identity: {
+      accessKey: "",
+      accountId: "",
+      apiKey: "",
+      apiKeyId: "",
+      caller: "",
+      clientCert: {
+        clientCertPem: "",
+        issuerDN: "",
+        serialNumber: "",
+        subjectDN: "",
+        validity: {notAfter: "", notBefore: ""}
+      },
+      cognitoAuthenticationProvider: "",
+      cognitoAuthenticationType: "",
+      cognitoIdentityId: "",
+      cognitoIdentityPoolId: "",
+      principalOrgId: "",
+      sourceIp: "",
+      user: "",
+      userAgent: "",
+      userArn: ""
+    },
+    path: "/hello",
+    protocol: "HTTP/1.1",
+    requestId: "c6af9ac6-7b61-11e6-9a41-93e8deadbeef",
+    requestTimeEpoch: 1428582896000,
+    resourceId: "123456",
+    resourcePath: "/hello",
+    stage: "dev"
+  },
+  resource: "",
+  stageVariables: {}
+}
 
 describe("Unit test for status check", function () {
   it("returns commit id from environment", async () => {
     process.env.COMMIT_ID = "test_commit_id"
     process.env.TargetSpineServer = "sandbox"
 
-    const event: Partial<APIGatewayProxyEvent> = {}
-    const result: APIGatewayProxyResult = (await handler(
-      event as APIGatewayProxyEvent,
-      dummyContext
-    )) as APIGatewayProxyResult
+    const result: APIGatewayProxyResult = (await handler(mockEvent, dummyContext)) as APIGatewayProxyResult
 
     expect(result.statusCode).toEqual(200)
     expect(JSON.parse(result.body)).toMatchObject({
@@ -26,15 +77,24 @@ describe("Unit test for status check", function () {
     process.env.VERSION_NUMBER = "test_version_number"
     process.env.TargetSpineServer = "sandbox"
 
-    const event: Partial<APIGatewayProxyEvent> = {}
-    const result: APIGatewayProxyResult = (await handler(
-      event as APIGatewayProxyEvent,
-      dummyContext
-    )) as APIGatewayProxyResult
+    const result: APIGatewayProxyResult = (await handler(mockEvent, dummyContext)) as APIGatewayProxyResult
 
     expect(result.statusCode).toEqual(200)
     expect(JSON.parse(result.body)).toMatchObject({
       versionNumber: "test_version_number"
+    })
+  })
+  it("appends trace id's to the logger", async () => {
+    const mockAppendKeys = jest.spyOn(Logger.prototype, "appendKeys")
+
+    await handler(mockEvent, dummyContext)
+
+    expect(mockAppendKeys).toHaveBeenCalledWith({
+      "nhsd-correlation-id": "test-request-id.test-correlation-id.rrt-5789322914740101037-b-aet2-20145-482635-2",
+      "x-request-id": "test-request-id",
+      "nhsd-request-id": "test-request-id",
+      "x-correlation-id": "test-correlation-id",
+      "apigw-request-id": "c6af9ac6-7b61-11e6-9a41-93e8deadbeef"
     })
   })
 })
