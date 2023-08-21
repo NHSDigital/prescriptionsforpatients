6bc6318c5c0ff0717247295ce398c96258d5eba9
Jack Spagnoli <114365987+JackSpagnoliNHS@users.noreply.github.com>
2023-08-21 14:32:17 +0000

    adds common package and moves gateway proxy event

---
+++ packages/common/src/index.ts
@@ -0,0 +1,6 @@
+import {APIGatewayProxyEvent} from "aws-lambda"
+import * as _mockAPIGatewayProxyEvent from "./events/mockAPIGatewayProxyEvent.json"
+
+const mockAPIGatewayProxyEvent: APIGatewayProxyEvent = _mockAPIGatewayProxyEvent
+
+export {mockAPIGatewayProxyEvent}
