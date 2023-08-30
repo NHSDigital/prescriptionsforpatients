ab48a4ab82fa16ed8474289c306cca96825a71ef
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2023-08-30 15:39:03 +0000

    return top level status on status endpoint

--- packages/statusLambda/tests/test-handler.test.ts
+++ packages/statusLambda/tests/test-handler.test.ts
@@ -3,6 +3,10 @@ import {handler} from "../src/app"
 import {expect, describe, it} from "@jest/globals"
 import {ContextExamples} from "@aws-lambda-powertools/commons"
 import {Logger} from "@aws-lambda-powertools/logger"
+import MockAdapter from "axios-mock-adapter"
+import axios from "axios"
+
+const mock = new MockAdapter(axios)
 
 const dummyContext = ContextExamples.helloworldContext
 const mockEvent: APIGatewayProxyEvent = {
@@ -61,6 +65,10 @@ const mockEvent: APIGatewayProxyEvent = {
 }
 
 describe("Unit test for status check", function () {
+  afterEach(() => {
+    mock.reset()
+  })
+
   it("returns commit id from environment", async () => {
     process.env.COMMIT_ID = "test_commit_id"
     process.env.TargetSpineServer = "sandbox"
@@ -110,5 +118,61 @@ describe("Unit test for status check", function () {
     expect(headers).toMatchObject({
       "Cache-Control": "no-cache"
     })
+  })
+
+  it("returns success when spine check succeeds", async () => {
+    mock.onGet("https://live/healthcheck").reply(200, {})
+    process.env.TargetSpineServer = "live"
+
+    const result: APIGatewayProxyResult = (await handler(mockEvent, dummyContext)) as APIGatewayProxyResult
+
+    expect(result.statusCode).toEqual(200)
+    const result_body = JSON.parse(result.body)
+    expect(result_body.status).toEqual("pass")
+    expect(result_body.spineStatus.status).toEqual("pass")
+    expect(result_body.spineStatus.timeout).toEqual("false")
+    expect(result_body.spineStatus.responseCode).toEqual(200)
+  })
+
+  it("returns failure when spine check fails", async () => {
+    mock.onGet("https://live/healthcheck").reply(500, {})
+    process.env.TargetSpineServer = "live"
+
+    const result: APIGatewayProxyResult = (await handler(mockEvent, dummyContext)) as APIGatewayProxyResult
+
+    expect(result.statusCode).toEqual(200)
+    const result_body = JSON.parse(result.body)
+    expect(result_body.status).toEqual("error")
+    expect(result_body.spineStatus.status).toEqual("error")
+    expect(result_body.spineStatus.timeout).toEqual("false")
+    expect(result_body.spineStatus.responseCode).toEqual(500)
+  })
+
+  it("returns failure when spine check has network error", async () => {
+    mock.onGet("https://live/healthcheck").networkError()
+    process.env.TargetSpineServer = "live"
+
+    const result: APIGatewayProxyResult = (await handler(mockEvent, dummyContext)) as APIGatewayProxyResult
+
+    expect(result.statusCode).toEqual(200)
+    const result_body = JSON.parse(result.body)
+    expect(result_body.status).toEqual("error")
+    expect(result_body.spineStatus.status).toEqual("error")
+    expect(result_body.spineStatus.timeout).toEqual("false")
+    expect(result_body.spineStatus.responseCode).toEqual(500)
+  })
+
+  it("returns failure when spine check has timeout", async () => {
+    mock.onGet("https://live/healthcheck").timeout()
+    process.env.TargetSpineServer = "live"
+
+    const result: APIGatewayProxyResult = (await handler(mockEvent, dummyContext)) as APIGatewayProxyResult
+
+    expect(result.statusCode).toEqual(200)
+    const result_body = JSON.parse(result.body)
+    expect(result_body.status).toEqual("error")
+    expect(result_body.spineStatus.status).toEqual("error")
+    expect(result_body.spineStatus.timeout).toEqual("true")
+    expect(result_body.spineStatus.responseCode).toEqual(500)
   })
 })
