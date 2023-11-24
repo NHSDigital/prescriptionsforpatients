b95734c8d17f08fde9a044347b1eb8ba01d7eb48
phil <originalphil@gmail.com>
2023-11-24 17:01:28 +0000

    AEA-3450 Test data for interaction and service search.

--- packages/common/testing/src/index.ts
+++ packages/common/testing/src/index.ts
@@ -6,6 +6,15 @@ import {test_append_trace_ids} from "./test_append_trace_id"
 import _mockAPIGatewayProxyEvent from "./mockAPIGatewayProxyEvent.json"
 const mockAPIGatewayProxyEvent: APIGatewayProxyEvent = _mockAPIGatewayProxyEvent
 
+import _mockServiceSearchResponseBody from "./mockServiceSearchResponseBody.json"
+const mockServiceSearchResponseBody = _mockServiceSearchResponseBody
+
+import _mockInteractionResponseBody from "./mockInteractionResponseBody.json"
+const mockInteractionResponseBody = _mockInteractionResponseBody
+
 export {mockAPIGatewayProxyEvent}
+export {mockServiceSearchResponseBody}
+export {mockInteractionResponseBody}
+
 export {test_mime_type}
 export {test_append_trace_ids}
