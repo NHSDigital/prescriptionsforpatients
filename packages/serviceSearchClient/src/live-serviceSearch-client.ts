656805706ae3ab31ce1750b747e99c3940361f2c
Phil Gee <originalphil@gmail.com>
2023-11-28 16:54:54 +0000

    AEA-3450 Trimmed-down live client and tests.

--- packages/serviceSearchClient/src/live-serviceSearch-client.ts
+++ packages/serviceSearchClient/src/live-serviceSearch-client.ts
@@ -1,53 +1,38 @@
 import {Logger} from "@aws-lambda-powertools/logger"
-import {serviceHealthCheck, StatusCheckResponse} from "./status"
 import {ServiceSearchClient} from "./serviceSearch-client"
 import {Agent} from "https"
-import axios, {AxiosResponse} from "axios"
-import {APIGatewayProxyEventHeaders} from "aws-lambda"
-import {extractNHSNumber} from "./extractNHSNumber"
+import axios from "axios"
+import {validateUrl} from "./validateUrl"
+import {StatusCheckResponse, serviceHealthCheck} from "./status"
+
+type ServiceSearchResponse = {serviceUrl: string, isDistanceSelling: boolean, urlValid: boolean}
 
 // timeout in ms to wait for response from serviceSearch to avoid lambda timeout
 const SERVICE_SEARCH_TIMEOUT = 45000
+const DISTANCE_SELLING = "DistanceSelling"
 export class LiveServiceSearchClient implements ServiceSearchClient {
   private readonly SERVICE_SEARCH_URL_SCHEME = "https"
-  private readonly SERVICE_SEARCH_ENDPOINT = process.env.TargetSpineServer
-  private readonly serviceSearchASID: string | undefined
+  private readonly SERVICE_SEARCH_ENDPOINT = process.env.TargetServiceSearchServer
   private readonly httpsAgent: Agent
-  private readonly serviceSearchPartyKey: string | undefined
 
   constructor() {
-    this.serviceSearchASID = process.env.SpineASID
-    this.serviceSearchPartyKey = process.env.SpinePartyKey
-
     this.httpsAgent = new Agent({
-      cert: process.env.SpinePublicCertificate,
-      key: process.env.SpinePrivateKey,
-      ca: process.env.SpineCAChain
+      cert: process.env.ServiceSearchPublicCertificate,
+      key: process.env.ServiceSearchPrivateKey,
+      ca: process.env.ServiceSearchCAChain
     })
   }
-  async getPrescriptions(inboundHeaders: APIGatewayProxyEventHeaders, logger: Logger): Promise<AxiosResponse> {
+  async searchService(odsCode: string, logger: Logger): Promise<ServiceSearchResponse> {
     try {
-      const address = this.getSpineEndpoint("mm/patientfacingprescriptions")
-      // nhsd-nhslogin-user looks like P9:9912003071
-      const nhsNumber = extractNHSNumber(inboundHeaders["nhsd-nhslogin-user"])
-      logger.info(`nhsNumber: ${nhsNumber}`)
-
+      const address = this.getServiceSearchEndpoint("service-search")
       const outboundHeaders = {
-        Accept: "application/json",
-        "Spine-From-Asid": this.serviceSearchASID,
-        "nhsd-party-key": this.serviceSearchPartyKey,
-        nhsNumber: nhsNumber,
-        "nhsd-correlation-id": inboundHeaders["nhsd-correlation-id"],
-        "nhsd-nhslogin-user": inboundHeaders["nhsd-nhslogin-user"],
-        "x-request-id": inboundHeaders["x-request-id"],
-        "x-correlation-id": inboundHeaders["x-correlation-id"],
-        "nhsd-request-id": inboundHeaders["nhsd-request-id"]
+        Accept: "application/json"
       }
 
       const queryParams = {
-        format: "trace-summary"
+        search: odsCode
       }
-      logger.info(`making request to ${address}`)
+      logger.info(`making request to ${address} with ods code ${odsCode}`)
       const response = await axios.get(address, {
         headers: outboundHeaders,
         params: queryParams,
@@ -55,22 +40,20 @@ export class LiveServiceSearchClient implements ServiceSearchClient {
         timeout: SERVICE_SEARCH_TIMEOUT
       })
 
-      // This can be removed when https://nhsd-jira.digital.nhs.uk/browse/AEA-3448 is complete
-      if (
-        response.data["statusCode"] !== undefined &&
-        response.data["statusCode"] !== "1" &&
-        response.data["statusCode"] !== "0"
-      ) {
-        logger.error("Unsuccessful status code response from serviceSearch", {
-          response: {
-            data: response.data,
-            status: response.status,
-            Headers: response.headers
-          }
-        })
-        throw new Error("Unsuccessful status code response from serviceSearch")
+      const isDistanceSelling = response.data["OrganisationSubType"] === DISTANCE_SELLING
+      const serviceUrl = response.data["URL"]
+      let urlValid = false
+
+      if (isDistanceSelling) {
+        logger.info(`service with ods code ${odsCode} is of type ${DISTANCE_SELLING}`)
+        urlValid = validateUrl(serviceUrl, logger)
       }
-      return response
+
+      return {
+        serviceUrl: serviceUrl,
+        isDistanceSelling: isDistanceSelling,
+        urlValid: urlValid
+      }
     } catch (error) {
       if (axios.isAxiosError(error)) {
         if (error.response) {
@@ -105,13 +88,13 @@ export class LiveServiceSearchClient implements ServiceSearchClient {
     }
   }
 
-  private getSpineEndpoint(requestPath?: string) {
+  private getServiceSearchEndpoint(requestPath?: string) {
     return `${this.SERVICE_SEARCH_URL_SCHEME}://${this.SERVICE_SEARCH_ENDPOINT}/${requestPath}`
   }
 
   async getStatus(logger: Logger): Promise<StatusCheckResponse> {
     if (process.env.healthCheckUrl === undefined) {
-      return serviceHealthCheck(this.getSpineEndpoint("healthcheck"), logger, this.httpsAgent)
+      return serviceHealthCheck(this.getServiceSearchEndpoint("healthcheck"), logger, this.httpsAgent)
     } else {
       return serviceHealthCheck(process.env.healthCheckUrl, logger, new Agent())
     }
@@ -120,9 +103,11 @@ export class LiveServiceSearchClient implements ServiceSearchClient {
   isCertificateConfigured(): boolean {
     // Check if the required certificate-related environment variables are defined
     return (
-      process.env.SpinePublicCertificate !== "ChangeMe" &&
-      process.env.SpinePrivateKey !== "ChangeMe" &&
-      process.env.SpineCAChain !== "ChangeMe"
+      process.env.ServiceSearchPublicCertificate !== "ChangeMe" &&
+      process.env.ServiceSearchPrivateKey !== "ChangeMe" &&
+      process.env.ServiceSearchCAChain !== "ChangeMe"
     )
   }
 }
+
+export {ServiceSearchResponse}
--- packages/serviceSearchClient/tests/live-serviceSearch-client.test.ts
+++ packages/serviceSearchClient/tests/live-serviceSearch-client.test.ts
@@ -1,16 +1,13 @@
-import {LiveServiceSearchClient} from "../src/live-serviceSearch-client"
+import {LiveServiceSearchClient, ServiceSearchResponse} from "../src/live-serviceSearch-client"
 import "jest"
 import MockAdapter from "axios-mock-adapter"
 import axios from "axios"
 import {Logger} from "@aws-lambda-powertools/logger"
-import {APIGatewayProxyEventHeaders} from "aws-lambda"
 
 const mock = new MockAdapter(axios)
-process.env.TargetSpineServer = "serviceSearch"
+process.env.TargetServiceSearchServer = "serviceSearch"
 type serviceSearchFailureTestData = {
   httpResponseCode: number
-  serviceSearchStatusCode: string
-  nhsdLoginUser: string | undefined
   errorMessage: string
   scenarioDescription: string
 }
@@ -22,99 +19,45 @@ describe("live serviceSearch client", () => {
     mock.reset()
   })
 
-  test("successful response when http response is status 200 and serviceSearch status does not exist", async () => {
-    mock.onGet("https://serviceSearch/mm/patientfacingprescriptions").reply(200, {resourceType: "Bundle"})
+  test("successful response when http response is status 200 and serviceUrl exists", async () => {
+    const response = {value: {URL: "https://www.pharmacy2u.co.uk", OrganisationSubType: "DistanceSelling"}}
+    mock.onGet("https://serviceSearch/service-search").reply(200, response)
     const serviceSearchClient = new LiveServiceSearchClient()
-    const headers: APIGatewayProxyEventHeaders = {
-      "nhsd-nhslogin-user": "P9:9912003071"
-    }
-    const serviceSearchResponse = await serviceSearchClient.getPrescriptions(headers, logger)
+    const serviceData = await serviceSearchClient.searchService("", logger)
 
-    expect(serviceSearchResponse.status).toBe(200)
-    expect(serviceSearchResponse.data).toStrictEqual({resourceType: "Bundle"})
+    const expected: ServiceSearchResponse = {
+      serviceUrl: "https://www.pharmacy2u.co.uk", isDistanceSelling: true, urlValid: true
+    }
+
+    expect(serviceData).toStrictEqual(expected)
   })
 
   test.each<serviceSearchFailureTestData>([
     {
       httpResponseCode: 200,
-      serviceSearchStatusCode: "99",
-      nhsdLoginUser: "P9:9912003071",
       errorMessage: "Unsuccessful status code response from serviceSearch",
       scenarioDescription: "serviceSearch returns a non successful response status"
-    },
-    {
-      httpResponseCode: 500,
-      serviceSearchStatusCode: "0",
-      nhsdLoginUser: "P9:9912003071",
-      errorMessage: "Request failed with status code 500",
-      scenarioDescription: "serviceSearch returns an unsuccessful http status code"
-    },
-    {
-      httpResponseCode: 200,
-      serviceSearchStatusCode: "0",
-      nhsdLoginUser: undefined,
-      errorMessage: "nhsdloginUser not passed in",
-      scenarioDescription: "no nhsd-login-user is passed in"
-    },
-    {
-      httpResponseCode: 200,
-      serviceSearchStatusCode: "0",
-      nhsdLoginUser: "P9:A",
-      errorMessage: "NHS Number failed preflight checks",
-      scenarioDescription: "nhs number in nhsdLoginUser contains a string"
-    },
-    {
-      httpResponseCode: 200,
-      serviceSearchStatusCode: "0",
-      nhsdLoginUser: "P9:123",
-      errorMessage: "NHS Number failed preflight checks",
-      scenarioDescription: "nhs number in nhsdLoginUser is too short"
-    },
-    {
-      httpResponseCode: 200,
-      serviceSearchStatusCode: "0",
-      nhsdLoginUser: "P0:9912003071",
-      errorMessage: "Identity proofing level is not P9",
-      scenarioDescription: "Identity proofing in nhsdLoginUser is not P9"
-    },
-    {
-      httpResponseCode: 200,
-      serviceSearchStatusCode: "0",
-      nhsdLoginUser: "P9:9912003072",
-      errorMessage: "invalid check digit in NHS number",
-      scenarioDescription: "nhs number does not validate checksum"
     }
   ])(
     "throw error when $scenarioDescription",
-    async ({httpResponseCode, serviceSearchStatusCode, nhsdLoginUser, errorMessage}) => {
-      mock.onGet("https://serviceSearch/mm/patientfacingprescriptions").reply(
-        httpResponseCode, {statusCode: serviceSearchStatusCode}
+    async ({httpResponseCode, errorMessage}) => {
+      mock.onGet("https://serviceSearch/service-search").reply(
+        httpResponseCode, {value: {URL: "", OrganisationSubType: ""}}
       )
       const serviceSearchClient = new LiveServiceSearchClient()
-      const headers: APIGatewayProxyEventHeaders = {
-        "nhsd-nhslogin-user": nhsdLoginUser
-      }
-      await expect(serviceSearchClient.getPrescriptions(headers, logger)).rejects.toThrow(errorMessage)
+      await expect(serviceSearchClient.searchService("", logger)).rejects.toThrow(errorMessage)
     }
   )
 
   test("should throw error when unsuccessful http request", async () => {
-    mock.onGet("https://serviceSearch/mm/patientfacingprescriptions").networkError()
-
+    mock.onGet("https://serviceSearch/service-search").networkError()
     const serviceSearchClient = new LiveServiceSearchClient()
-    const headers: APIGatewayProxyEventHeaders = {
-      "nhsd-nhslogin-user": "P9:9912003071"
-    }
-    await expect(serviceSearchClient.getPrescriptions(headers, logger)).rejects.toThrow("Network Error")
+    await expect(serviceSearchClient.searchService("", logger)).rejects.toThrow("Network Error")
   })
 
   test("should throw error when timout on http request", async () => {
-    mock.onGet("https://serviceSearch/mm/patientfacingprescriptions").timeout()
-
+    mock.onGet("https://serviceSearch/service-search").timeout()
     const serviceSearchClient = new LiveServiceSearchClient()
-    const headers: APIGatewayProxyEventHeaders = {
-      "nhsd-nhslogin-user": "P9:9912003071"
-    }
-    await expect(serviceSearchClient.getPrescriptions(headers, logger)).rejects.toThrow("timeout of 45000ms exceeded")
+    await expect(serviceSearchClient.searchService("", logger)).rejects.toThrow("timeout of 45000ms exceeded")
   })
 })
