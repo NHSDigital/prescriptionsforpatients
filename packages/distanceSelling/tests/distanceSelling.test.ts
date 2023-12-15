a1e4d5a8a025b00174f58ed6f75bacfbb7b06ad5
Phil Gee <originalphil@gmail.com>
2023-12-15 11:49:34 +0000

    AEA-3450 Test for adding undefined values to cache.

--- packages/distanceSelling/tests/distanceSelling.test.ts
+++ packages/distanceSelling/tests/distanceSelling.test.ts
@@ -5,7 +5,7 @@ import {
   ContactPoint,
   Organization
 } from "fhir/r4"
-import {DistanceSelling} from "../src/distanceSelling"
+import {DistanceSelling, ServicesCache} from "../src/distanceSelling"
 import {mockInteractionResponseBody, mockServiceSearchResponseBody} from "@prescriptionsforpatients_common/testing"
 import MockAdapter from "axios-mock-adapter"
 import axios from "axios"
@@ -168,14 +168,33 @@ describe("ServiceSearch tests", function () {
     expect(organisation.telecom![1]).toEqual(expectedTelecom)
   })
 
-  it("processOdsCodes doesn't call service search when cache entry exists for ODS code", async () => {
-    const distanceSelling = new DistanceSelling({"flm49": "https://www.pharmacy2u.co.uk/"})
+  it("processOdsCode doesn't call service search when cache entry exists for ODS code", async () => {
+    const distanceSellingWithCache = new DistanceSelling({"flm49": "https://www.pharmacy2u.co.uk/"})
     const searchsetBundle = JSON.parse(mockBundleString) as Bundle
 
-    const startMockCallCount: number = mock.history.get.length
-    await distanceSelling.search(searchsetBundle)
-    const endMockCallCount: number = mock.history.get.length
+    const prescriptions = distanceSellingWithCache.isolatePrescriptions(searchsetBundle)
+    const performerReferences = distanceSellingWithCache.getPerformerReferences(prescriptions)
+    const organisations = distanceSellingWithCache.getPerformerOrganisations(performerReferences, prescriptions)
 
-    expect(startMockCallCount).toEqual(endMockCallCount)
+    await distanceSellingWithCache.processOdsCodes(organisations)
+
+    expect(mock.history.get.length).toEqual(0)
+  })
+
+  it("searchOdsCode adds empty item to cache when no url returned by service search", async () => {
+    mock.onGet("https://live/service-search").reply(200, {value: []})
+    const servicesCache: ServicesCache = {}
+    const odsCode = "flm49"
+    const distanceSelling = new DistanceSelling(servicesCache)
+    const searchsetBundle = JSON.parse(mockBundleString) as Bundle
+
+    const prescriptions = distanceSelling.isolatePrescriptions(searchsetBundle)
+    const performerReferences = distanceSelling.getPerformerReferences(prescriptions)
+    const organisation = distanceSelling.getPerformerOrganisations(performerReferences, prescriptions)[0]
+
+    await distanceSelling.searchOdsCode(odsCode, organisation)
+
+    expect(odsCode in servicesCache).toBeTruthy()
+    expect(servicesCache[odsCode]).toEqual(undefined)
   })
 })
