460c6d3e3ed40330d045a48085fd6ec46f663feb
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-01-16 07:48:17 +0000

    add some comments

--- packages/getMyPrescriptions/tests/toMatchJsonLogMessage.ts
+++ packages/getMyPrescriptions/tests/toMatchJsonLogMessage.ts
@@ -3,6 +3,12 @@ import {expect} from "@jest/globals"
 import type {MatcherFunction} from "expect"
 import * as _ from "lodash-es"
 
+/*
+This is a custom matcher that extends jest expect
+Its used for matching console log messages where the message sent to console is pretty printed JSON
+It converts the console log message to JSON, then compares an expected field in the JSON to expected value
+It also checks a field in actual value does not exist
+*/
 const toMatchJsonLogMessage: MatcherFunction<[jsonField: unknown, jsonValue: unknown, missingJsonField: unknown]> =
   function (actual, jsonField, jsonValue, missingJsonField) {
     if (
@@ -25,7 +31,7 @@ const toMatchJsonLogMessage: MatcherFunction<[jsonField: unknown, jsonValue: unk
         message: () => `expected JSON log message ${this.utils.printReceived(
           actualJson,
         )} to have ${this.utils.printExpected(
-          `${jsonField} with value ${jsonValue}`,
+          `${jsonField} with value ${jsonValue} and ${missingJsonField} to not exist`,
         )}`,
         pass: false
       }
