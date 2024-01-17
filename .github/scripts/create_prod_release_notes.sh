02a65ddd576e637c46c66fad4c021488578bcade
Phil Gee <originalphil@gmail.com>
2024-01-17 15:24:07 +0000

    AEA-3622 Using new scripts in create_confluence_release_notes workflow.

--- .github/scripts/create_prod_release_notes.sh
+++ .github/scripts/create_prod_release_notes.sh
@@ -1,1 +1,21 @@
 #!/usr/bin/env bash
+
+# shellcheck disable=SC2154
+
+ENV_VAR=release-notes:CreateReleaseNotesLambdaName
+RELEASE_NOTES_LAMBDA=$(aws cloudformation list-exports --query "Exports[?Name=='$ENV_VAR'].Value" --output text)
+
+cat <<EOF > payload.json
+{ 
+  "currentTag": "$target_tag",
+  "targetTag": "$dev_tag",
+  "repoName": "prescriptionsforpatients",
+  "targetEnvironment": "PROD",
+  "productName": "Prescritpions for Patients AWS layer",
+  "releaseNotesPageId": "693750029",
+  "releaseNotesPageTitle": "Current PfP AWS layer release notes - PROD"
+}
+EOF
+cat payload.json
+
+aws lambda invoke --function-name "${RELEASE_NOTES_LAMBDA}" --cli-binary-format raw-in-base64-out --payload file://payload.json out.txt
