91df93b3a93cbe4a920c54b375393b4b110eacdd
Phil Gee <originalphil@gmail.com>
2024-01-17 16:54:27 +0000

    AEA-3622 Error in cd caught by Ant.

--- .github/scripts/release_code.sh
+++ .github/scripts/release_code.sh
@@ -14,5 +14,5 @@ TRUSTSTORE_BUCKET_NAME=$(echo "${TRUSTSTORE_BUCKET_ARN}" | cut -d ":" -f 6)
 LATEST_TRUSTSTORE_VERSION=$(aws s3api list-object-versions --bucket "${TRUSTSTORE_BUCKET_NAME}" --prefix "${TRUSTSTORE_FILE}" --query 'Versions[?IsLatest].[VersionId]' --output text)
 export LATEST_TRUSTSTORE_VERSION
 
-cd ../../aws-sam/build || exit
+cd ../../.aws-sam/build || exit
 make sam-deploy-package
