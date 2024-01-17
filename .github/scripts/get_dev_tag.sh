2cce8717554a0970051c86467b6a8a5e78aa4389
Phil Gee <originalphil@gmail.com>
2024-01-17 17:03:31 +0000

    AEA-3622 Removed redundant files.

--- .github/scripts/get_dev_tag.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-dev_tag=$(aws cloudformation describe-stacks --stack-name dev-ci --query "Stacks[0].Tags[?Key=='version'].Value" --output text)
-export dev_tag
-echo "dev_tag=${dev_tag}" >> "$GITHUB_ENV"
