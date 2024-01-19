2243e73e2bf71ffbd73e203b0f033318ff0c5e32
Phil Gee <originalphil@gmail.com>
2024-01-19 14:58:38 +0000

    AEA-3622 Consolidated env release notes scripts.

--- .github/scripts/create_env_release_notes.sh
+++ .github/scripts/create_env_release_notes.sh
@@ -5,10 +5,10 @@ cat <<EOF > payload.json
   "currentTag": "$CURRENT_DEPLOYED_TAG",
   "targetTag": "$DEV_TAG",
   "repoName": "prescriptionsforpatients",
-  "targetEnvironment": "INT",
+  "targetEnvironment": "$ENV",
   "productName": "Prescriptions for Patients AWS layer",
-  "releaseNotesPageId": "693750027",
-  "releaseNotesPageTitle": "Current PfP AWS layer release notes - INT"
+  "releaseNotesPageId": "$PAGE_ID",
+  "releaseNotesPageTitle": "Current PfP AWS layer release notes - $ENV"
 }
 EOF
 cat payload.json
@@ -16,7 +16,7 @@ cat payload.json
 if [ "$DRY_RUN" != "true" ]; then
   function_arn=$(aws cloudformation list-exports --query "Exports[?Name=='release-notes:CreateReleaseNotesLambdaArn'].Value" --output text)
   aws lambda invoke --function-name "${function_arn}" \
-  --cli-binary-format raw-in-base64-out \
-  --payload file://payload.json out.txt
+    --cli-binary-format raw-in-base64-out \
+    --payload file://payload.json out.txt
   cat out.txt
 fi
--- .github/scripts/create_int_rc_release_notes.sh
+++ .github/scripts/create_int_rc_release_notes.sh
@@ -7,13 +7,14 @@ cat <<EOF > payload.json
   "repoName": "prescriptionsforpatients",
   "targetEnvironment": "INT",
   "productName": "Prescriptions for Patients AWS layer",
-  "releaseNotesPageId": "710051481",
+  "releaseNotesPageId": "$PAGE_ID",
   "releaseNotesPageTitle": "PfP-AWS-$RELEASE_TAG - Deployed to [INT] on $(date +'%d-%m-%y')",
   "createReleaseCandidate": "true",
   "releasePrefix": "PfP-AWS-"
 }
 EOF
 cat payload.json
+
 if [ "$DRY_RUN" != "true" ]; then
   function_arn=$(aws cloudformation list-exports --query "Exports[?Name=='release-notes:CreateReleaseNotesLambdaArn'].Value" --output text)
   aws lambda invoke --function-name "${function_arn}" \
