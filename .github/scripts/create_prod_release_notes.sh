3d3c385e51d038f395c15a5102e5c84623949074
Phil Gee <originalphil@gmail.com>
2024-01-19 14:46:07 +0000

    AEA-3622 Added create_prod_release_notes script.

--- .github/scripts/create_prod_release_notes.sh
+++ .github/scripts/create_prod_release_notes.sh
@@ -5,18 +5,18 @@ cat <<EOF > payload.json
   "currentTag": "$CURRENT_DEPLOYED_TAG",
   "targetTag": "$DEV_TAG",
   "repoName": "prescriptionsforpatients",
-  "targetEnvironment": "INT",
+  "targetEnvironment": "PROD",
   "productName": "Prescriptions for Patients AWS layer",
-  "releaseNotesPageId": "693750027",
-  "releaseNotesPageTitle": "Current PfP AWS layer release notes - INT"
+  "releaseNotesPageId": "693750029",
+  "releaseNotesPageTitle": "Current PfP AWS layer release notes - PROD"
 }
 EOF
-cat payload.json
 
+cat payload.json
 if [ "$DRY_RUN" != "true" ]; then
   function_arn=$(aws cloudformation list-exports --query "Exports[?Name=='release-notes:CreateReleaseNotesLambdaArn'].Value" --output text)
   aws lambda invoke --function-name "${function_arn}" \
-  --cli-binary-format raw-in-base64-out \
-  --payload file://payload.json out.txt
+    --cli-binary-format raw-in-base64-out \
+    --payload file://payload.json out.txt
   cat out.txt
 fi
