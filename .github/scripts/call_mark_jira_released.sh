9a906a4e92c717f0efb364d2f890033115718bdd
Phil Gee <originalphil@gmail.com>
2024-01-23 15:18:20 +0000

    AEA-3622 Tidying in mark_jira_released action.

--- .github/scripts/call_mark_jira_released.sh
+++ .github/scripts/call_mark_jira_released.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 echo "calling mark jira released"
+
 cat <<EOF > payload.json
 { 
   "releaseVersion": "PfP-AWS-$RELEASE_TAG"
@@ -10,8 +11,6 @@ cat payload.json
 
 if [ "$DRY_RUN" != "true" ]; then
   function_arn=$(aws cloudformation list-exports --query "Exports[?Name=='release-notes:MarkJiraReleasedLambdaArn'].Value" --output text)
-  aws lambda invoke --function-name "${function_arn}" \
-  --cli-binary-format raw-in-base64-out \
-  --payload file://payload.json out.txt
+  aws lambda invoke --function-name "${function_arn}" --cli-binary-format raw-in-base64-out --payload file://payload.json out.txt
   cat out.txt
 fi
