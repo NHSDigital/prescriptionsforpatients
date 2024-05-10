c8db3d280d33b357bb482fdef5840f2bc7a72d85
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-05-10 07:56:48 +0000

    set default value on vars

--- .github/scripts/release_code.sh
+++ .github/scripts/release_code.sh
@@ -13,5 +13,15 @@ TRUSTSTORE_BUCKET_NAME=$(echo "${TRUSTSTORE_BUCKET_ARN}" | cut -d ":" -f 6)
 LATEST_TRUSTSTORE_VERSION=$(aws s3api list-object-versions --bucket "${TRUSTSTORE_BUCKET_NAME}" --prefix "${TRUSTSTORE_FILE}" --query 'Versions[?IsLatest].[VersionId]' --output text)
 export LATEST_TRUSTSTORE_VERSION
 
+if [ -z "${DOMAIN_NAME_EXPORT}" ];
+then
+  DOMAIN_NAME_EXPORT="NOT SET"
+fi
+
+if [ -z "${ZONE_ID_EXPORT}" ];
+then
+  ZONE_ID_EXPORT="NOT SET"
+fi
+
 cd ../../.aws-sam/build || exit
 make sam-deploy-package
