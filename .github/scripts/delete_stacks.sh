e69d55ef53f21685bc4e248d49a83c98c3d6f0f9
anthony-nhs <121869075+anthony-nhs@users.noreply.github.com>
2024-05-15 13:34:57 +0100

    Fix: [AEA-0000] - fix delete stacks (#893)
    ## Summary
    
    - Routine Change
    
    ### Details
    
    - add a delay between deleting stacks to avoid 429 rate limiting when
    they are deleted
    - use a function for deleting stacks so the list of stacks is parsed
    correctly
    - add extra replace as sandbox could be at start or end of stack name

--- .github/scripts/delete_stacks.sh
+++ .github/scripts/delete_stacks.sh
@@ -1,25 +1,34 @@
 #!/usr/bin/env bash
 
+
+delete_stacks () {
+  ACTIVE_STACKS="$1"
+  mapfile -t ACTIVE_STACKS_ARRAY <<< "$ACTIVE_STACKS"
+  for i in "${ACTIVE_STACKS_ARRAY[@]}"
+  do 
+    echo "Checking if stack $i has open pull request"
+    PULL_REQUEST=${i//pfp-pr-/}
+    PULL_REQUEST=${PULL_REQUEST//pr-}
+    PULL_REQUEST=${PULL_REQUEST//sandbox-/}
+    PULL_REQUEST=${PULL_REQUEST//-sandbox/}
+    echo "Checking pull request id ${PULL_REQUEST}"
+    URL="https://api.github.com/repos/NHSDigital/prescriptionsforpatients/pulls/${PULL_REQUEST}"
+    RESPONSE=$(curl "${URL}" 2>/dev/null)
+    STATE=$(echo "${RESPONSE}" | jq -r .state)
+    if [ "$STATE" == "closed" ]; then
+      echo "** going to delete stack $i as state is ${STATE} **"
+      aws cloudformation delete-stack --stack-name "${i}"
+      echo "** Sleeping for 60 seconds to avoid 429 on delete stack **"
+      sleep 60
+    else
+      echo "not going to delete stack $i as state is ${STATE}"
+    fi
+  done
+}
+
+
 ACTIVE_STACKS=$(aws cloudformation list-stacks | jq -r '.StackSummaries[] | select ( .StackStatus != "DELETE_COMPLETE" ) | select( .StackName | capture("^pfp-pr-(\\d+)(-sandbox)?$") ) | .StackName ')
 OLD_ACTIVE_STACKS=$(aws cloudformation list-stacks | jq -r '.StackSummaries[] | select ( .StackStatus != "DELETE_COMPLETE" ) | select( .StackName | capture("^pr-(sandbox-)?(\\d+)$") ) | .StackName ')
-ALL_STACKS="$ACTIVE_STACKS $OLD_ACTIVE_STACKS"
 
-mapfile -t ACTIVE_STACKS_ARRAY <<< "$ALL_STACKS"
-
-for i in "${ACTIVE_STACKS_ARRAY[@]}"
-do 
-  echo "Checking if stack $i has open pull request"
-  PULL_REQUEST=${i//pfp-pr-/}
-  PULL_REQUEST=${PULL_REQUEST//pr-}
-  PULL_REQUEST=${PULL_REQUEST//sandbox-/}
-  echo "Checking pull request id ${PULL_REQUEST}"
-  URL="https://api.github.com/repos/NHSDigital/prescriptionsforpatients/pulls/${PULL_REQUEST}"
-  RESPONSE=$(curl "${URL}" 2>/dev/null)
-  STATE=$(echo "${RESPONSE}" | jq -r .state)
-  if [ "$STATE" == "closed" ]; then
-    echo "** going to delete stack $i as state is ${STATE} **"
-    aws cloudformation delete-stack --stack-name "${i}"
-  else
-    echo "not going to delete stack $i as state is ${STATE}"
-  fi
-done
+delete_stacks "${ACTIVE_STACKS}"
+delete_stacks "${OLD_ACTIVE_STACKS}"
