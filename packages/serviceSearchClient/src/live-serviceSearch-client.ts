cba2f486ab3e854c58fc395af5e7d0544cc8bcf9
Kris Szlapa <kris.szlapa1@nhs.net>
2024-07-25 15:24:44 +0000

    Ensure that error is treated as an AxiosError when calling stripApiKeyFromHeaders

--- packages/serviceSearchClient/src/live-serviceSearch-client.ts
+++ packages/serviceSearchClient/src/live-serviceSearch-client.ts
@@ -4,7 +4,7 @@ import axios, {AxiosError, AxiosInstance} from "axios"
 import axiosRetry from "axios-retry"
 import {handleUrl} from "./handleUrl"
 
-// timeout in ms to wait for response from serviceSearch to avoid lambda timeout
+// Timeout in ms to wait for response from serviceSearch to avoid lambda timeout
 export const SERVICE_SEARCH_TIMEOUT = 45000
 const DISTANCE_SELLING = "DistanceSelling"
 
@@ -61,7 +61,34 @@ export class LiveServiceSearchClient implements ServiceSearchClient {
               odsCode: error.config?.params?.search,
               timeout: SERVICE_SEARCH_TIMEOUT
             })
+          } else {
+            this.logger.error("error in request to serviceSearch", {
+              error: error.message,
+              code: error.code,
+              stack: error.stack,
+              response: error.response
+                ? {
+                  data: error.response.data,
+                  status: error.response.status,
+                  headers: error.response.headers
+                }
+                : undefined,
+              request: error.request
+                ? {
+                  method: error.request.method,
+                  url: error.request.url,
+                  headers: error.request.headers
+                }
+                : undefined
+            })
           }
+        } else {
+          this.logger.error("general error", {error: (error as Error).message, stack: (error as Error).stack})
+        }
+
+        // Strip API key from headers
+        if (axios.isAxiosError(error)) {
+          this.stripApiKeyFromHeaders(error)
         }
 
         return Promise.reject(error)
@@ -84,7 +111,7 @@ export class LiveServiceSearchClient implements ServiceSearchClient {
     const address = this.getServiceSearchEndpoint()
     const queryParams = {...this.baseQueryParams, search: odsCode}
 
-    this.logger.info(`making request to ${address} with ods code ${odsCode}`, {odsCode: odsCode})
+    this.logger.info(`making request to ${address} with ods code ${odsCode}`, {odsCode})
 
     try {
       const response = await this.axiosInstance.get(address, {
@@ -99,12 +126,12 @@ export class LiveServiceSearchClient implements ServiceSearchClient {
         return undefined
       }
 
-      this.logger.info(`pharmacy with ods code ${odsCode} is of type ${DISTANCE_SELLING}`, {odsCode: odsCode})
+      this.logger.info(`pharmacy with ods code ${odsCode} is of type ${DISTANCE_SELLING}`, {odsCode})
       const service = services[0]
-      const urlString = service["URL"]
+      const urlString = service.URL
 
       if (urlString === null) {
-        this.logger.warn(`ods code ${odsCode} has no URL but is of type ${DISTANCE_SELLING}`, {odsCode: odsCode})
+        this.logger.warn(`ods code ${odsCode} has no URL but is of type ${DISTANCE_SELLING}`, {odsCode})
         return undefined
       }
       const serviceUrl = handleUrl(urlString, odsCode, this.logger)
@@ -113,7 +140,7 @@ export class LiveServiceSearchClient implements ServiceSearchClient {
       if (axios.isAxiosError(error)) {
         if (error.code === "ECONNABORTED") {
           this.logger.error("serviceSearch request timed out", {
-            odsCode: odsCode,
+            odsCode,
             timeout: SERVICE_SEARCH_TIMEOUT,
             message: error.message,
             stack: error.stack
@@ -139,14 +166,18 @@ export class LiveServiceSearchClient implements ServiceSearchClient {
               : undefined
           })
         }
+
+        // Strip API key from headers
+        this.stripApiKeyFromHeaders(error)
       } else {
         this.logger.error("general error", {error: (error as Error).message, stack: (error as Error).stack})
       }
+
       throw error
     }
   }
 
-  stripApiKeyFromHeaders(error: AxiosError) {
+  private stripApiKeyFromHeaders(error: AxiosError) {
     const headerKey = "subscription-key"
     if (error.response?.headers) {
       delete error.response.headers[headerKey]
