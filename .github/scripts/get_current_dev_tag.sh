d0b129639b5721916258f7d126946b25c3d6ba01
Jack Spagnoli <Jack.Spagnoli1@nhs.net>
2024-08-15 12:17:17 +0000

    cleans scripts

--- .github/scripts/get_current_dev_tag.sh
+++ .github/scripts/get_current_dev_tag.sh
@@ -2,4 +2,9 @@
 
 dev_tag=$(aws cloudformation describe-stacks --stack-name dev-ci --query "Stacks[0].Tags[?Key=='version'].Value" --output text)
 
+if [ -z "$current_deployed_tag" ]
+then
+     current_deployed_tag=v1.0.970-beta
+fi
+
 echo "DEV_TAG=${dev_tag}" >> "$GITHUB_ENV"
