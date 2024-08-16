55c19723a834054b01457839aaf0ab13b0ca3fcd
Jack Spagnoli <jack.spagnoli1@nhs.net>
2024-08-16 10:49:04 +0100

    moves null handling from get_current_dev_tag.sh to get_target_deployed_tag.sh

--- .github/scripts/get_target_deployed_tag.sh
+++ .github/scripts/get_target_deployed_tag.sh
@@ -2,4 +2,9 @@
 
 current_deployed_tag=$(aws cloudformation describe-stacks --stack-name "$TARGET_ENVIRONMENT"-ci --query "Stacks[0].Tags[?Key=='version'].Value" --output text)
 
+if [ -z "$current_deployed_tag" ]
+then
+     current_deployed_tag=v1.0.970-beta
+fi
+
 echo "CURRENT_DEPLOYED_TAG=${current_deployed_tag}" >> "$GITHUB_ENV"
