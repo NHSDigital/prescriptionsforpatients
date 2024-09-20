b246e1f04ae133acab0ae18975ab191b538b90ad
anthony-nhs <121869075+anthony-nhs@users.noreply.github.com>
2024-09-20 13:06:55 +0100

    Docs: [AEA-0000] - fix getting tag for release notes generation (#1302)
    ## Summary
    
    - Routine Change
    
    ### Details
    
    - get tag from new stack name

--- .github/scripts/get_current_dev_tag.sh
+++ .github/scripts/get_current_dev_tag.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-dev_tag=$(aws cloudformation describe-stacks --stack-name dev-ci --query "Stacks[0].Tags[?Key=='version'].Value" --output text)
+dev_tag=$(aws cloudformation describe-stacks --stack-name pfp --query "Stacks[0].Tags[?Key=='version'].Value" --output text)
 
 echo "DEV_TAG=${dev_tag}" >> "$GITHUB_ENV"
