6dc74a76ef82c455da87e0d4a25359dc36a060f6
anthony-nhs <121869075+anthony-nhs@users.noreply.github.com>
2024-10-08 09:56:59 +0100

    Fix: [AEA-0000] - remove zone id and domainname  variables as not needed now (#1354)
    ## Summary
    
    - Routine Change
    
    ### Details
    
    - remove unneeded zoneid and domainname variables on sandbox stack

--- .github/scripts/release_code.sh
+++ .github/scripts/release_code.sh
@@ -13,15 +13,5 @@ TRUSTSTORE_BUCKET_NAME=$(echo "${TRUSTSTORE_BUCKET_ARN}" | cut -d ":" -f 6)
 LATEST_TRUSTSTORE_VERSION=$(aws s3api list-object-versions --bucket "${TRUSTSTORE_BUCKET_NAME}" --prefix "${TRUSTSTORE_FILE}" --query 'Versions[?IsLatest].[VersionId]' --output text)
 export LATEST_TRUSTSTORE_VERSION
 
-if [ -z "${DOMAIN_NAME_EXPORT}" ];
-then
-  export DOMAIN_NAME_EXPORT="NOT_SET"
-fi
-
-if [ -z "${ZONE_ID_EXPORT}" ];
-then
-  export ZONE_ID_EXPORT="NOT_SET"
-fi
-
 cd ../../.aws-sam/build || exit
 make sam-deploy-package
