5a165b23db42bb4349df66654e887dd4cc392860
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2025-08-01 08:33:13 +0000

    more tags

--- .github/scripts/release_code.sh
+++ .github/scripts/release_code.sh
@@ -14,4 +14,38 @@ LATEST_TRUSTSTORE_VERSION=$(aws s3api list-object-versions --bucket "${TRUSTSTOR
 export LATEST_TRUSTSTORE_VERSION
 
 cd ../../.aws-sam/build || exit
-make sam-deploy-package
+
+REPO=prescriptionsforpatients
+CFN_DRIFT_DETECTION_GROUP="pfp"
+if [[ "$STACK_NAME" =~ -pr-[0-9]+$ ]]; then
+  CFN_DRIFT_DETECTION_GROUP="pfp-pull-request"
+fi
+
+sam deploy \
+  --template-file "$TEMPLATE_FILE" \
+  --stack-name "$STACK_NAME" \
+  --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
+  --region eu-west-2 \
+  --s3-bucket "$artifact_bucket" \
+  --s3-prefix "$ARTIFACT_BUCKET_PREFIX" \
+  --config-file samconfig_package_and_deploy.toml \
+  --no-fail-on-empty-changeset \
+  --role-arn "$cloud_formation_execution_role" \
+  --no-confirm-changeset \
+  --force-upload \
+  --tags "version=$VERSION_NUMBER stack=$STACK_NAME repo=$REPO cfnDriftDetectionGroup=$CFN_DRIFT_DETECTION_GROUP" \
+  --parameter-overrides \
+      TruststoreVersion="$LATEST_TRUSTSTORE_VERSION" \
+      TruststoreFile="$TRUSTSTORE_FILE" \
+      EnableMutualTLS="$ENABLE_MUTUAL_TLS" \
+      TargetSpineServer="$TARGET_SPINE_SERVER" \
+      TargetServiceSearchServer="$TARGET_SERVICE_SEARCH_SERVER" \
+      EnableSplunk=true \
+      VersionNumber="$VERSION_NUMBER" \
+      CommitId="$COMMT_ID" \
+      LogLevel="$LOG_LEVEL" \
+      LogRetentionInDays="$LOG_RETENTION_DAYS" \
+      Env="$TARGET_ENVIRONMENT" \
+      ToggleGetStatusUpdates="$TOGGLE_GET_STATUS_UPDATES" \
+      EnableAlerts="$ENABLE_ALERTS" \
+      StateMachineLogLevel="$STATE_MACHINE_LOG_LEVEL"
