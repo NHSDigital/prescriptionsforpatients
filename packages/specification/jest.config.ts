2ffc014a3dd163a088566d6d1b872ec211eb8b25
Connor Avery <214469360+connoravo-nhs@users.noreply.github.com>
2025-12-11 09:37:53 +0000

    WIP - Modify pipeline & include full spec
    Signed-off-by: Connor Avery <214469360+connoravo-nhs@users.noreply.github.com>

---
+++ packages/specification/jest.config.ts
@@ -0,0 +1,9 @@
+import defaultConfig from "../../jest.default.config"
+import type {JestConfigWithTsJest} from "ts-jest"
+
+const jestConfig: JestConfigWithTsJest = {
+  ...defaultConfig,
+  "rootDir": "./"
+}
+
+export default jestConfig
