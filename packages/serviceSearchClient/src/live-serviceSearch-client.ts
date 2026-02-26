ac498871233bf138a1dd50bbb369e84ec10a97f1
tstephen-nhs <231503406+tstephen-nhs@users.noreply.github.com>
2026-02-26 16:41:53 +0000

    chore: remove double logging in func and interceptor

--- packages/serviceSearchClient/src/live-serviceSearch-client.ts
+++ packages/serviceSearchClient/src/live-serviceSearch-client.ts
@@ -1,6 +1,11 @@
 import {Logger} from "@aws-lambda-powertools/logger"
 import {getSecret} from "@aws-lambda-powertools/parameters/secrets"
-import axios, {AxiosError, AxiosInstance} from "axios"
+import axios, {
+  AxiosError,
+  AxiosHeaders,
+  AxiosInstance,
+  isAxiosError
+} from "axios"
 import axiosRetry, {isNetworkOrIdempotentRequestError} from "axios-retry"
 import {handleUrl} from "./handleUrl"
 
@@ -105,8 +110,18 @@ export class LiveServiceSearchClient implements ServiceSearchClient {
 
       // reject with a proper Error object
       let err: Error
-      if (error instanceof AxiosError) {
-        this.logger.error("Axios error in serviceSearch request", {error})
+      if (isAxiosError(error)) {
+        this.stripApiKeyFromHeaders(error)
+        this.logger.error("Axios error in serviceSearch request", {
+          axiosErrorDetails: {
+            request: error.request,
+            response: {
+              data: error.response?.data,
+              status: error.response?.status,
+              headers: error.response?.headers as AxiosHeaders
+            }
+          }
+        })
         err = new Error(`Axios error in serviceSearch request: ${error.message}`)
       } else if (error instanceof Error) {
         this.logger.error("Error in serviceSearch request", {error})
@@ -152,67 +167,38 @@ export class LiveServiceSearchClient implements ServiceSearchClient {
   }
 
   async searchService(odsCode: string, correlationId: string): Promise<URL | undefined> {
-    try {
-      // Load API key if not set in environment (secrets layer is failing to load v3 key)
-      const apiVsn = getServiceSearchVersion(this.logger)
-      if (apiVsn === 3 && !this.outboundHeaders.apikey) {
-        this.logger.info("API key not in environment, attempting to load from Secrets Manager")
-        this.outboundHeaders.apikey = await this.loadApiKeyFromSecretsManager()
+    // Load API key if not set in environment (secrets layer is failing to load v3 key)
+    const apiVsn = getServiceSearchVersion(this.logger)
+    if (apiVsn === 3 && !this.outboundHeaders.apikey) {
+      this.logger.info("API key not in environment, attempting to load from Secrets Manager")
+      this.outboundHeaders.apikey = await this.loadApiKeyFromSecretsManager()
+    }
+    this.outboundHeaders["x-correlation-id"] = correlationId
+    const xRequestId = crypto.randomUUID()
+    this.outboundHeaders["x-request-id"] = xRequestId
+
+    const address = getServiceSearchEndpoint(this.logger)
+    const queryParams = {...SERVICE_SEARCH_BASE_QUERY_PARAMS, search: odsCode}
+
+    this.logger.info(`making request to ${address} with ods code ${odsCode}`, {
+      odsCode: odsCode,
+      requestHeaders: {
+        "x-request-id": xRequestId,
+        "x-correlation-id": correlationId
       }
-      this.outboundHeaders["x-correlation-id"] = correlationId
-      const xRequestId = crypto.randomUUID()
-      this.outboundHeaders["x-request-id"] = xRequestId
+    })
+    const response = await this.axiosInstance.get(address, {
+      headers: this.outboundHeaders,
+      params: queryParams,
+      timeout: SERVICE_SEARCH_TIMEOUT
+    })
 
-      const address = getServiceSearchEndpoint(this.logger)
-      const queryParams = {...SERVICE_SEARCH_BASE_QUERY_PARAMS, search: odsCode}
-
-      this.logger.info(`making request to ${address} with ods code ${odsCode}`, {
-        odsCode: odsCode,
-        requestHeaders: {
-          "x-request-id": xRequestId,
-          "x-correlation-id": correlationId
-        }
-      })
-      const response = await this.axiosInstance.get(address, {
-        headers: this.outboundHeaders,
-        params: queryParams,
-        timeout: SERVICE_SEARCH_TIMEOUT
-      })
-
-      this.logger.info(`received response from serviceSearch for ods code ${odsCode}`,
-        {odsCode: odsCode, status: response.status, data: response.data})
-      if (apiVsn === 2) {
-        return this.handleV2Response(odsCode, response.data)
-      } else {
-        return this.handleV3Response(odsCode, response.data)
-      }
-
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        this.stripApiKeyFromHeaders(error)
-        if (error.response) {
-          this.logger.error("error in response from serviceSearch", {
-            response: {
-              data: error.response.data,
-              status: error.response.status,
-              Headers: error.response.headers
-            },
-            request: {
-              method: error.request?.path,
-              params: error.request?.params,
-              headers: error.request?.headers,
-              host: error.request?.host
-            }
-          })
-        } else if (error.request) {
-          this.logger.error("error in request to serviceSearch", {error})
-        } else {
-          this.logger.error("general error calling serviceSearch", {error})
-        }
-      } else {
-        this.logger.error("general error", {error})
-      }
-      throw error
+    this.logger.info(`received response from serviceSearch for ods code ${odsCode}`,
+      {odsCode: odsCode, status: response.status, data: response.data})
+    if (apiVsn === 2) {
+      return this.handleV2Response(odsCode, response.data)
+    } else {
+      return this.handleV3Response(odsCode, response.data)
     }
   }
   handleV3Response(odsCode: string, data: ServiceSearch3Data): URL | undefined {
