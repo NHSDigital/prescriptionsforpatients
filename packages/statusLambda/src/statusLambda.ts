d54b79d6e087bf06a91bd40c7ee8ebbfdc3bc3bd
tstephen-nhs <231503406+tstephen-nhs@users.noreply.github.com>
2026-03-11 09:01:20 +0000

    chore: adapt to middy type change

--- packages/statusLambda/src/statusLambda.ts
+++ packages/statusLambda/src/statusLambda.ts
@@ -52,7 +52,7 @@ export const handler = middy(lambdaHandler)
   .use(
     inputOutputLogger({
       logger: (request) => {
-        logger.info(request)
+        logger.info("inputOutputLogger request", {request})
       }
     })
   )
