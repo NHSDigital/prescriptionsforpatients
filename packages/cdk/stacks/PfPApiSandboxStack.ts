d70a0f6fef90ef190644d4eaf0d92ee19c0d881d
tstephen-nhs <231503406+tstephen-nhs@users.noreply.github.com>
2026-03-19 17:54:55 +0000

    feat: scaffold app with cdk

---
+++ packages/cdk/stacks/PfPApiSandboxStack.ts
@@ -0,0 +1,10 @@
+import {Stack, App} from "aws-cdk-lib"
+import {StandardStackProps} from "@nhsdigital/eps-cdk-constructs"
+
+export class PfPApiSandboxStack extends Stack {
+  public constructor(scope: App, id: string, props: StandardStackProps){
+    super(scope, id, props)
+
+    // PLACEHOLDER FOR SANDBOX RESOURCES
+  }
+}
---
+++ packages/cdk/stacks/PfPApiStack.ts
@@ -0,0 +1,57 @@
+import {App, Stack} from "aws-cdk-lib"
+import {nagSuppressions} from "../nagSuppressions"
+import {Functions} from "../resources/Functions"
+import {StateMachines} from "../resources/StateMachines"
+import {Apis} from "../resources/Apis"
+import {StandardStackProps} from "@nhsdigital/eps-cdk-constructs"
+
+export interface PfPApiStackProps extends StandardStackProps {
+  readonly stackName: string
+  readonly logRetentionInDays: number
+  readonly logLevel: string
+  readonly targetSpineServer: string
+  readonly targetServiceSearchServer: string
+  readonly toggleGetStatusUpdates: string
+  readonly allowNhsNumberOverride: string
+  readonly mutualTlsTrustStoreKey: string | undefined
+  readonly csocApiGatewayDestination: string
+  readonly forwardCsocLogs: boolean
+}
+
+export class PfPApiStack extends Stack {
+  public constructor(scope: App, id: string, props: PfPApiStackProps){
+    super(scope, id, props)
+
+    // Resources
+    const functions = new Functions(this, "Functions", {
+      stackName: props.stackName,
+      version: props.version,
+      commitId: props.commitId,
+      deploymentEnvironment: props.environment,
+      targetSpineServer: props.targetSpineServer,
+      targetServiceSearchServer: props.targetServiceSearchServer,
+      toggleGetStatusUpdates: props.toggleGetStatusUpdates,
+      allowNhsNumberOverride: props.allowNhsNumberOverride,
+      logRetentionInDays: props.logRetentionInDays,
+      logLevel: props.logLevel
+    })
+
+    const stateMachines = new StateMachines(this, "StateMachines", {
+      stackName: props.stackName,
+      logRetentionInDays: props.logRetentionInDays,
+      functions: functions.functions
+    })
+
+    new Apis(this, "Apis", {
+      stackName: props.stackName,
+      logRetentionInDays: props.logRetentionInDays,
+      mutualTlsTrustStoreKey: props.mutualTlsTrustStoreKey,
+      functions: functions.functions,
+      stateMachines: stateMachines.stateMachines,
+      csocApiGatewayDestination: props.csocApiGatewayDestination,
+      forwardCsocLogs: props.forwardCsocLogs
+    })
+
+    nagSuppressions(this)
+  }
+}
